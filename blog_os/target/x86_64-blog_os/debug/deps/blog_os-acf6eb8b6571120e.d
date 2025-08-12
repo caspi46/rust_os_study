efbb27a2cb5eb7febb63eb139f22d771067d2714
Kenny Kim <caspi4646@gmail.com>
2025-08-11 22:27:10 -0700

    able to read the keyboard inputs

---
+++ blog_os/target/x86_64-blog_os/debug/deps/blog_os-acf6eb8b6571120e.d
@@ -0,0 +1,11 @@
+/Users/kennykim/Documents/rust_os_study/blog_os/target/x86_64-blog_os/debug/deps/blog_os-acf6eb8b6571120e.d: src/lib.rs src/serial.rs src/vga_buffer.rs src/interrupts.rs src/gdt.rs
+
+/Users/kennykim/Documents/rust_os_study/blog_os/target/x86_64-blog_os/debug/deps/libblog_os-acf6eb8b6571120e.rlib: src/lib.rs src/serial.rs src/vga_buffer.rs src/interrupts.rs src/gdt.rs
+
+/Users/kennykim/Documents/rust_os_study/blog_os/target/x86_64-blog_os/debug/deps/libblog_os-acf6eb8b6571120e.rmeta: src/lib.rs src/serial.rs src/vga_buffer.rs src/interrupts.rs src/gdt.rs
+
+src/lib.rs:
+src/serial.rs:
+src/vga_buffer.rs:
+src/interrupts.rs:
+src/gdt.rs:
---
+++ blog_os/target/x86_64-blog_os/debug/deps/blog_os-ae8d7d5dab6d39af.d
@@ -0,0 +1,6 @@
+/Users/kennykim/Documents/rust_os_study/blog_os/target/x86_64-blog_os/debug/deps/blog_os-ae8d7d5dab6d39af.d: src/main.rs src/serial.rs
+
+/Users/kennykim/Documents/rust_os_study/blog_os/target/x86_64-blog_os/debug/deps/blog_os-ae8d7d5dab6d39af: src/main.rs src/serial.rs
+
+src/main.rs:
+src/serial.rs:
